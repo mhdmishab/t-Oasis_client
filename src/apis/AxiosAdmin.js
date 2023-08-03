fd5b218dcb9d993ca199304ca9c870e99d0c37b1
mhdmishab <mishabbtt63@gmail.com>
2023-08-03 17:13:27 +0530

    urlupdatedtodomain

--- src/apis/AxiosAdmin.js
+++ src/apis/AxiosAdmin.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://toasis.restinpillows.shop";
 
 const axiosPublic = axios.create({
   baseURL: BASE_URL,
